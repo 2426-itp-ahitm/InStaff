9db5e189d499421a09d9e9dc0a653e69cd065eac
KlementTobias <t.klement@students.htl-leonding.ac.at>
2026-03-05 14:30:27 +0100

    changed dashboard from welcome to instaff to welcome to [username]

--- frontend/src/app/dashboard/employee-dashboard/employee-dashboard.component.ts
+++ frontend/src/app/dashboard/employee-dashboard/employee-dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, ViewChild} from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {CalendarComponent} from '../../essentials/calendar/calendar.component';
 import {NewsComponent} from '../../news/news/news.component';
 import {ShiftServiceService} from '../../shift/shift-service/shift-service.service';
@@ -6,6 +6,9 @@ import {Shift} from '../../interfaces/shift';
 import {EmployeeCalendarComponent} from '../employee-calendar/employee-calendar.component';
 import {NgIf} from '@angular/common';
 import {ShiftViewComponent} from '../../shift/shift-view/shift-view.component';
+import {EmployeeServiceService} from '../../employee/employee-service/employee-service.service';
+import {KeycloakService} from 'keycloak-angular';
+import {EmployeeShiftOverviewComponent} from '../employee-shift-overview/employee-shift-overview.component';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -14,18 +17,31 @@ import {ShiftViewComponent} from '../../shift/shift-view/shift-view.component';
     NewsComponent,
     EmployeeCalendarComponent,
     NgIf,
-    ShiftViewComponent
+    ShiftViewComponent,
+    EmployeeShiftOverviewComponent
   ],
   templateUrl: './employee-dashboard.component.html',
   styleUrl: './employee-dashboard.component.css'
 })
-export class EmployeeDashboardComponent {
+export class EmployeeDashboardComponent implements OnInit {
   @ViewChild(CalendarComponent) calendar!: EmployeeCalendarComponent;
   shiftService: ShiftServiceService = inject(ShiftServiceService);
+  employeeService: EmployeeServiceService = inject(EmployeeServiceService);
+  keycloackService: KeycloakService = inject(KeycloakService);
 
   selectedShift!: Shift;
   isViewMode: boolean = false;
 
+  userName: string = "";
+
+  ngOnInit(): void {
+    this.employeeService.getEmployeeByKeycloakId(this.keycloackService.getKeycloakInstance().subject!).subscribe((emp) => {
+      this.userName = emp.firstname + ' ' + emp.lastname;
+      if(emp == null){
+        this.userName = "bei Instaff";
+      }
+    });
+  }
 
   openShiftViewWithId(shiftId: number) {
     console.log(shiftId);
--- frontend/src/app/dashboard/manager-dashboard/manager-dashboard.component.ts
+++ frontend/src/app/dashboard/manager-dashboard/manager-dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, ViewChild} from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {CalendarComponent} from '../../essentials/calendar/calendar.component';
 import {ShiftServiceService} from '../../shift/shift-service/shift-service.service';
 import {Shift} from '../../interfaces/shift';
@@ -9,6 +9,8 @@ import {ShiftEditComponent} from '../../shift/shift-edit/shift-edit.component';
 import {ShiftViewComponent} from '../../shift/shift-view/shift-view.component';
 import {ManagerCalendarComponent} from '../manager-calendar/manager-calendar.component';
 import {ShiftCreateDTO} from '../../interfaces/new-shift';
+import {EmployeeServiceService} from '../../employee/employee-service/employee-service.service';
+import {KeycloakService} from 'keycloak-angular';
 
 
 @Component({
@@ -25,14 +27,23 @@ import {ShiftCreateDTO} from '../../interfaces/new-shift';
   templateUrl: './manager-dashboard.component.html',
   styleUrl: './manager-dashboard.component.css'
 })
-export class ManagerDashboardComponent {
+export class ManagerDashboardComponent implements OnInit {
   @ViewChild(CalendarComponent) calendar!: CalendarComponent;
   shiftService: ShiftServiceService = inject(ShiftServiceService);
+  employeeService: EmployeeServiceService = inject(EmployeeServiceService);
+  keycloackService: KeycloakService = inject(KeycloakService);
 
   selectedShift!: Shift;
   isEditMode: boolean = false;
   isAddMode: boolean = false;
 
+  userName: string = "";
+
+  ngOnInit(): void {
+    this.employeeService.getEmployeeByKeycloakId(this.keycloackService.getKeycloakInstance().subject!).subscribe((emp) => {
+      this.userName = emp.firstname + ' ' + emp.lastname;
+    });
+  }
   openShiftEditWithId(shiftId: number) {
     console.log(shiftId);
     this.shiftService.getShiftById(shiftId).subscribe((shift: Shift) => {
