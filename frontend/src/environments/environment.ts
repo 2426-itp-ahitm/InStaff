cab890c4ff9c62a6f2ee269cf7604c3ba8061f71
alexanderhahn1 <a.hahn@students.htl-leonding.ac.at>
2026-03-05 10:07:21 +0100

    commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: "http://localhost:8080/api",
-  keycloakUrl: "http://localhost:8081",
+  keycloakUrl: "https://it210157.cloud.htl-leonding.ac.at/auth",
   wsUrl: "ws://localhost:8080/api/api/ws",
 };
